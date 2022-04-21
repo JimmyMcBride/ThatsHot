cf42d8b2e90aca0aeb3d8777ea055f13a00657cd
jimmymcbride <mcbride967@gmail.com>
2022-04-21 13:57:41 -0400

    Testing initial pull request

--- app/src/main/java/com/example/thatshot/view/AddRecipeFragment.kt
+++ app/src/main/java/com/example/thatshot/view/AddRecipeFragment.kt
@@ -1,19 +1,26 @@
 package com.example.thatshot.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.thatshot.R
 import com.example.thatshot.adapter.IngredientListAdapter
+import com.example.thatshot.databinding.ActivityMainBinding
 import com.example.thatshot.databinding.FragmentAddRecipeBinding
 import com.example.thatshot.repo.models.DummyIngredient
+
 
 class AddRecipeFragment : Fragment() {
     private var _binding: FragmentAddRecipeBinding? = null
     private val binding get() = _binding!!
-//    private val viewModel by activityViewModels<>()
+//    private val viewModel by viewModel<>()
 
     private var ingredients = mutableListOf<DummyIngredient>()
 
@@ -26,19 +33,45 @@ class AddRecipeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setHasOptionsMenu(true) //Set this to true in order to trigger callbacks to Fragment#onOptionsItemSelected
+
+//        (requireActivity() as AppCompatActivity).apply {
+//            // Redirect system "Back" press to our dispatcher
+//            onBackPressedDispatcher.addCallback(viewLifecycleOwner, backPressedDispatcher)
+//
+//            // Set toolbar if it is in Fragment's layout. If you have a global toolbar that lives in Activity layout, then you don't need this line.
+//            setSupportActionBar(ActivityMainBinding.toolbar)
+//
+//            // Setup action bar to work with NavController
+//            setupActionBarWithNavController(findNavController())
+//        }
+
+
         initViews()
     }
 
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return if (item.itemId == android.R.id.home) {
+//            // Redirect "Up/Home" button clicks to our own function
+//            this@AddRecipeFragment.onBackPressed()
+//            true
+//        } else {
+//            super.onOptionsItemSelected(item)
+//        }
+//    }
+//
+//    private fun onBackPressed() {
+//        // Work your magic! Show dialog etc.
+//    }
+
     override fun onDestroyView() {
+//        backPressedDispatcher.remove()
         super.onDestroyView()
         _binding = null
     }
 
     private fun initViews() = with(binding) {
         rvIngredients.adapter = IngredientListAdapter(ingredients, false)
-        btnQuitRecipe.setOnClickListener {
-            findNavController().navigateUp()
-        }
         btnSaveRecipe.setOnClickListener {
             findNavController().navigateUp()
         }
